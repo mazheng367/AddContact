b3cee04bc2991e3177ff35dcb3dcb4fd744f4110
mazheng367@qq.com <mazheng367@qq.com>
2018-05-21 19:42:40 +0800

    添加项目文件。

---
+++ TestUploadServer/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestUploadServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Random r = new Random();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Options | HttpVerbs.Post)]
+        public ActionResult About()
+        {
+            var file = Request.Files[0];
+            return Json(new {success = true, fileId = r.Next(), filename = file?.FileName});
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TestUploadServer/OptionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestUploadServer
+{
+    public class OptionHandler:IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
